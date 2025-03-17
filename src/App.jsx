1f7562c711025258ad1e6ed1d5bc68abcb740134
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-17 16:22:02 +0000

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  //defining state without using useState
 
+  // let number = 0;
+
+  // //example of using useState
+  // const names = ['John', 'Doe', 'Jane'];
+  // names.forEach(name => {
+  //   console.log(name);
+  // })
+
+  //define state with useState
+  // const returnValue = useState();
+  // console.log(returnValue);
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    //updating a state without using useState
+    // number += 1;
+
+    //updating a state with useState
+    setNumber(number + 1)
+
+
+    //Log the updated state
+    console.log(number);
+  }
   return (
     <>
-      <div></div>
+      <button onClick={handleClick}>Current number: {number}</button>
     </>
   )
 }
